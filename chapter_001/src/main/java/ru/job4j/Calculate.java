29093405f35e0680d97293875d30c47c00045992
Roma-Rio <r.a.gilyazev@gmail.com>
2017-10-06 07:34:52 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j;
 
+/**
+ *Описание класса Calculate
+ *@author Ramil' Gilyazev 
+ *@version task 1.3
+ *@since 06.10.2017
+ */
 public  class Calculate{
-	
+	/**
+	*Конструктор,вывод  строки в консоль
+	*@param args-массив строк
+	*/
 	public static void main (String[] args){
 	System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Ramil' Gilyazev (mailto:r.a.gilyazev@gmail.com)
+* @version $Id$
+* @since 06.10.2017
+*/
+package ru.job4j;
