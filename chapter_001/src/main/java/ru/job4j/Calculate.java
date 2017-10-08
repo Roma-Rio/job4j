69cd0c98a9a98a99b24d198e7b0f54b24706ca7e
Roma-Rio <r.a.gilyazev@gmail.com>
2017-10-08 11:28:44 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,26 +1,29 @@
 package ru.job4j;
 
 /**
- *Описание класса Calculate.
+ *Описание класса Calculate
  *
- *@author Ramil' Gilyazev.
- *@version task 1.3.
- *@since 06.10.2017.
+ *@author Ramil' Gilyazev 
+ *@version task 1.3
+ *@since 06.10.2017
  */
-public  class Calculate {
+public  class Calculate{
 	/**
-	 * Main.
-	 * @param args - args.
-	 */
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	*Конструктор,вывод  строки в консоль
+	*@param args-массив строк
+	*/
+	public static void main (String[] args){
+	System.out.println("Hello World");
 	}
+	
 	/**
-	 * Method echo.
-	 * @param name Your name.
-	 * @return three Echo plus your name.
-	 */
-	public String echo(String name) {
-		return "Echo,echo,echo:" + name;
+	* Method echo.
+	* @param name Your name.
+	* @return three Echo plus your name.
+	*/
+	public String echo(String name){
+		return "Echo,echo,echo:"+name;
 	}
+	
 }
+	
